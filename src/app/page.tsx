50937136a2514856609c9c952d041e1df4d62536
YahaQ2 <vivopopupcera@gmail.com>
2024-12-24 17:25:21 +0700

    update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import bg from "../../public/background/home-background.png";
 import RenderModel from "@/components/RenderModel";
 import Navigation from "@/components/navigation";
 import Harry from "@/components/models/Harry";
-
-export default function Home() {
-  return (         <p className="font-light text-foreground text-lg">
+ <p className="font-light text-foreground text-lg">
             Hai selamat datang!! 
           </p>
+export default function Home() {
+  return (        
     <main className="flex min-h-screen flex-col items-center justify-between relative">
       <Image
         src={bg}
